e45f4888d464494dc9ae2772656e4dd507428784
ukmastercom <utkarsh70.uuk@gmail.com>
2023-08-13 23:13:03 +0530

    added transition

---
+++ main.js
@@ -0,0 +1,8 @@
+// import Typed from 'typed.js';
+var typed = new Typed(".text", {
+    strings: ["Software Developer", "Backend Developer", "Data Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
